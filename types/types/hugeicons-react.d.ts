4522662a0cf35855f58f2ecb3922ff6e10ff4374
Pracas Infosys <100864380+pracasupreti@users.noreply.github.com>
2026-02-24 14:29:11 +0545

    File added

---
+++ types/types/hugeicons-react.d.ts
@@ -0,0 +1,6 @@
+declare module 'hugeicons-react' {
+  import React from 'react';
+  export const HugeiconsIcon: React.FC<any>;
+  export const PaymentSuccess01Icon: React.FC<any>;
+  export const SeatSelectorIcon: React.FC<any>;
+}
